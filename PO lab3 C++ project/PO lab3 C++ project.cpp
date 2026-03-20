6736effd31b33f98bcf4b5ba670425853b75d15c
NIKKIN06 <05.naumov.07.2006@gmail.com>
2026-03-20 23:26:51 +0200

    made class, constructor, destructor and working_routine

--- PO lab3 C++ project/PO lab3 C++ project.cpp
+++ PO lab3 C++ project/PO lab3 C++ project.cpp
@@ -1,6 +1,91 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+using namespace std;
+
+class ThreadPool
+{
+private:
+    vector<thread> workers;
+    queue<function<void()>> tasks;
+
+    mutex queue_mutex;
+	condition_variable cv;
+
+	bool stop;
+
+    void worker_routine();
+public:
+	ThreadPool(size_t num_threads = 8);
+
+    ~ThreadPool();
+
+    bool is_stopped() const
+    {
+        return stop;
+    }
+
+    template<class F, class... Args> void enqueue(F&& f, Args&&... args);
+};
+
+void ThreadPool::worker_routine()
+{
+    while (true)
+    {
+        function<void()> task;
+
+        {
+            unique_lock<mutex> lock(queue_mutex);
+
+            cv.wait(lock, [this]() {
+				return stop || !tasks.empty();
+            });
+
+			if (stop && tasks.empty())
+            {
+                return;
+            }
+
+            task = move(tasks.front());
+            tasks.pop();
+        }
+
+        task();
+    }
+}
+
+ThreadPool::ThreadPool(size_t num_threads) : stop(false)
+{
+    for(size_t i = 0; i < num_threads; ++i)
+    {
+        workers.emplace_back(&ThreadPool::worker_routine, this);
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    {
+        unique_lock<mutex> lock(queue_mutex);
+        stop = true;
+    }
+
+    cv.notify_all();
+
+    for (thread& worker : workers)
+    {
+        if (worker.joinable())
+        {
+			worker.join();
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
